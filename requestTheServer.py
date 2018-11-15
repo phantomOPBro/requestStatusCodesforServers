502ba12ab4ce68fa031d69c5ac07865e723cfe96
PhantomOPBro <30814769+phantomOPBro@users.noreply.github.com>
2018-11-15 06:47:06 +0000

    Created File

---
+++ requestTheServer.py
@@ -0,0 +1,8 @@
+import requests
+a="10.1.33."
+for i in range(68,85):
+  try:
+    r=requests.get("http://"+str(a)+str(i))
+    print(r.status_code + i)
+  except requests.exceptions.RequestException as e:
+    continue
